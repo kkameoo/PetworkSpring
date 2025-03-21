6d18013e826be46c19e7c80daeb526f2124ee91e
kkameoo <khdg1202@naver.com>
2025-03-21 18:09:17 +0900

    FEAT: 포토 올리기 추가

--- src/main/java/com/himedia/services/PhotoService.java
+++ src/main/java/com/himedia/services/PhotoService.java
@@ -1,9 +1,16 @@
 package com.himedia.services;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.himedia.repository.vo.BoardPhotoVo;
 
 @Service
 public interface PhotoService {
-	
-
+	// 사진 업로드
+	String uploadProfilePicture(MultipartFile file, Integer id) throws IOException;
+	// 사진 조회
+	BoardPhotoVo getBoardPhoto(Integer id);
 }
