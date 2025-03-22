9d3b79b09e3d1fa2e350de5855061cda9c4c71d6
kkameoo <khdg1202@naver.com>
2025-03-22 16:48:53 +0900

    FEAT: 펫 이미지 업로드, 조회, 여러장 사진 조회 기능 추가

--- src/main/java/com/himedia/mappers/BoardPhotoMapper.java
+++ src/main/java/com/himedia/mappers/BoardPhotoMapper.java
@@ -1,5 +1,7 @@
 package com.himedia.mappers;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.himedia.repository.vo.BoardPhotoVo;
@@ -9,6 +11,6 @@ public interface BoardPhotoMapper {
 //	<insert id="insertOrUpdateBoardPhoto" parameterType="BoardPhotoVo">
 	int insertOrUpdateBoardPhoto(BoardPhotoVo boardPhotoVo);
 	
-	BoardPhotoVo getBoardPhotoById (Integer id);
+	List<BoardPhotoVo> getBoardPhotoById (Integer id);
 
 }
