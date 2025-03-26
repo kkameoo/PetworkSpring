13f82582f9554946d0048407e20d22f45d6cffda
kkameoo <khdg1202@naver.com>
2025-03-26 10:49:08 +0900

    FEAT: 글로벌 콜스 수정

--- src/main/java/com/himedia/configs/GlobalCorsConfig.java
+++ src/main/java/com/himedia/configs/GlobalCorsConfig.java
@@ -12,7 +12,7 @@ public class GlobalCorsConfig implements WebMvcConfigurer {
 //		CORS 정책 설정
 		registry.addMapping("/api/**")	
 		//	/api로 시작되는 모든 요청에 CORS 정책 부여
-			.allowedOrigins("http://localhost")	//	모든 도메인에 대해 허용
+			.allowedOrigins("http://localhost:5173")	//	모든 도메인에 대해 허용
 			.allowedMethods("GET", "POST", "PUT", "DELETE")
 			.allowedHeaders("*")	//	모든 헤더 허용
 			.maxAge(3600)	//	CORS 캐싱 허용 시간 (초단위)
